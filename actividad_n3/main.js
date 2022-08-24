0b21350ba744375df9d59823329e5bce41bfe09d
Guiisho <alarconguille556@gmail.com>
2022-08-24 17:05:56 -0300

    subo actividad 3

---
+++ actividad_n3/main.js
@@ -0,0 +1,20 @@
+import Cripto from "./cripto.js";
+
+
+let cripto = new Cripto()
+cripto.obtener_cripto()
+
+/*let filas = [];
+  lista_usuarios.forEach((element,index) => {
+    let fila = `
+        <tr>
+            <td>${index+1}</td>
+            <td>${element.name}</td>
+            <td>${element.username}</td>
+            <td>${element.email}</td>
+            <td>${element.website}</td>
+        </tr>
+    `;
+    filas.push(fila);
+    document.getElementById("tbl_body").innerHTML = filas.join("");
+  });*/
