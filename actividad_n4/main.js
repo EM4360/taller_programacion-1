43305a194a9bd816f10504aee8cd6884bd272fb6
Guiisho <alarconguille556@gmail.com>
2022-08-30 17:06:18 -0300

    cambios actividad n4

--- actividad_n4/main.js
+++ actividad_n4/main.js
@@ -1,4 +1,11 @@
 import Producto from "./producto.js";
 
-let producto = new Producto()
-producto.obtener_producto()
+function obtener(){
+    let producto = new Producto()
+
+    producto.categoria = document.getElementById("slt_operar").value
+    producto.obtener_producto()
+}
+
+
+document.getElementById("slt_operar").addEventListener("change",obtener)
--- actividad_n4/producto.js
+++ actividad_n4/producto.js
@@ -1,27 +1,34 @@
 export default class Producto {
+  
+  categoria;
+  
   constructor() {}
 
   async obtener_producto() {
-    const producto = await fetch("https://fakestoreapi.com/products");
+
+
+    const producto = await fetch("https://fakestoreapi.com/products/category/"+this.categoria);
     const lista_producto = await producto.json();
     let columnas = [];
     lista_producto.forEach((element) => {
       let columna = `
-            <div class="col-lg-4">
-                <div class="">           
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="${element.image}" alt="Card image cap">
+            <div class="col-lg-3">
+                  
+                  <div class="card" >
+                        <img height="330" class="card-img-top" src="${element.image}" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">${element.tittle}</h5>
+                            <h5 class="card-title">${element.title}</h5>
                             <p class="card-text">${element.price}</p>
                             <a href="#" class="btn btn-primary">Comprar</a>
                         </div>  
                     </div>
-                </div>    
+  
             </div>
         `;
-      columnas.push(columna);
+      columnas.push(columna)
       document.getElementById("row").innerHTML = columnas.join("");
     });
   }
+
+ 
 }
